c8e483fdb171d6fd0ee59ff1f81254b2ba9ba4c9
GitHub Action <action@github.com>
2021-12-10 01:42:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/aeolun/intellijmycoverage/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aeolun/intellijmycoverage/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.aeolun.intellijmycoverage.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aeolun.intellijmycoverage.MyBundle
 
 class MyProjectService(project: Project) {
 
